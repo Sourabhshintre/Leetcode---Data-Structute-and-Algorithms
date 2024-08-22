8b0edb7f4a0e0bb1056966a04fcecb3c99495495
Sourabh Shintre <96312514+Sourabhshintre@users.noreply.github.com>
2024-08-22 12:37:47 +0530

    Time: 3 ms (41.07%), Space: 7.7 MB (8.34%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int findComplement(int num) {
-    
-        std::bitset<32> binary(num);
+         std::bitset<32> binary(num);
         std::string binaryString = binary.to_string();
         
-   
+       
         int i = 0;
         while (i < binaryString.length() && binaryString[i] != '1') {
             i++;
         }
 
-     
+      
         std::string ans = "";
         for (int j = i; j < binaryString.length(); j++) {
             if (binaryString[j] == '1') {
