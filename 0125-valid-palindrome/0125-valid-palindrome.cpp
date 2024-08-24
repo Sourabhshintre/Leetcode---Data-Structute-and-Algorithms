23f7fcb3874c18b1bc492d679f55af8bcd8f2ba8
Sourabh Shintre <96312514+Sourabhshintre@users.noreply.github.com>
2024-08-25 00:12:00 +0530

    Time: 8 ms (34.11%), Space: 8.9 MB (51.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,18 +2,40 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         
-        string ans;
+    //     string ans;
 
-        for (char c : s) {
-        if (isalnum(c)) {  
-            ans += tolower(c); 
+    //     for (char c : s) {
+    //     if (isalnum(c)) {  
+    //         ans += tolower(c); 
+    //     }
+    // }
+    //     string temp=ans;
+    //     reverse(ans.begin(),ans.end());
+
+    //    return ans==temp;
+
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            // Move the left pointer forward if the current character is not alphanumeric
+            while (left < right && !std::isalnum(s[left])) {
+                left++;
+            }
+            // Move the right pointer backward if the current character is not alphanumeric
+            while (left < right && !std::isalnum(s[right])) {
+                right--;
+            }
+            // Compare the characters after converting them to lowercase
+            if (std::tolower(s[left]) != std::tolower(s[right])) {
+                return false;
+            }
+            // Move both pointers towards the center
+            left++;
+            right--;
         }
-    }
-         
+        
+        // If all characters matched, it's a palindrome
+        return true;
 
-        string temp=ans;
-        reverse(ans.begin(),ans.end());
-
-       return ans==temp;
     }
-};
+}; 
