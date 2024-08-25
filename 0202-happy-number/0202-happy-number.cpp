1000bb00e9ed010721c9df72e2a39429dda6731c
Sourabh Shintre <96312514+Sourabhshintre@users.noreply.github.com>
2024-08-25 14:11:24 +0530

    Time: 2 ms (49.17%), Space: 7.9 MB (51.63%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
+    bool isHappy(int n) {
 
-      bool isHappy(int n) {
-        std::unordered_set<int> seen;
-        
-        while (n != 1 && seen.find(n) == seen.end()) {
-            seen.insert(n);
-            n = getNext(n);
+        set<int> s;
+        int val;
+        int index;
+
+        while(1)
+        {
+            val = 0;
+            while(n)
+            {
+                index = n%10;
+                val += index*index;
+                n /= 10;
+            }
+
+            if(val == 1)
+                return true;
+            else if(s.find(val) != s.end())
+                return false;
+
+            s.insert(val);
+            n = val;
         }
+        return false;
         
-        return n == 1;
-    }
-private:
-    int getNext(int n) {
-        int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
-        }
-        return sum;
     }
 };
