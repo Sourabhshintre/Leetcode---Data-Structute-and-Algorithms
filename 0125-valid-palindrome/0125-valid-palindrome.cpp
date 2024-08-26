3cff32180cfb76165baed8e043771856c4447054
Sourabh Shintre <96312514+Sourabhshintre@users.noreply.github.com>
2024-08-26 11:06:18 +0530

    Time: 4 ms (79.76%), Space: 8.7 MB (81.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -17,24 +17,24 @@ public:
         int left = 0, right = s.size() - 1;
 
         while (left < right) {
-            // Move the left pointer forward if the current character is not alphanumeric
+           
             while (left < right && !std::isalnum(s[left])) {
                 left++;
             }
-            // Move the right pointer backward if the current character is not alphanumeric
+         
             while (left < right && !std::isalnum(s[right])) {
                 right--;
             }
-            // Compare the characters after converting them to lowercase
+            
             if (std::tolower(s[left]) != std::tolower(s[right])) {
                 return false;
             }
-            // Move both pointers towards the center
+           
             left++;
             right--;
         }
         
-        // If all characters matched, it's a palindrome
+     
         return true;
 
     }
